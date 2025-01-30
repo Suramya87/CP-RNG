d5295a76e28e3bffd7fa41cbc6d8b7b914fac717
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-01-29 22:17:46 -0800

    3/4 finished
    finally had a chance to do the coding challenge

--- src/Play.js
+++ src/Play.js
@@ -11,6 +11,8 @@ class Play extends Phaser.Scene {
         this.SHOT_VELOCITY_Y_MIN = 700
         this.SHOT_VELOCITY_Y_MAX =1100
 
+
+
     }
 
     preload() {
@@ -32,15 +34,32 @@ class Play extends Phaser.Scene {
         this.cup.body.setOffset(this.cup.width /4)
         this.cup.body.setImmovable(true)
         // add ball
+        const BALLS = () => {
         this.ball = this.physics.add.sprite(width / 2, height-height/10, 'ball')
         this.ball.body.setCircle(this.ball.width /2)
         this.ball.body.setCollideWorldBounds(true)
         this.ball.body.setBounce(0.5)
         this.ball.body.setDamping(true).setDrag(0.5)
-        // add walls
-        let wallA = this.physics.add.sprite(0,height/4, 'wall')
-        wallA.setX(Phaser.Math.Between(0 + wallA.width /2, width - wallA.width /2))
-        wallA.body.setImmovable(true)
+        // cup/ball collision
+        this.physics.add.collider(this.ball, this.cup, (ball,cup)=>{
+            ball.destroy()
+            BALLS()
+        })
+
+        // ball/wall collision
+        this.physics.add.collider(this.ball, this.walls)
+
+        // ball/one-way collision
+        this.physics.add.collider(this.ball, this.oneWay)
+        };
+
+        let wallA = this.physics.add.sprite(0, height / 4, 'wall');
+        wallA.setX(Phaser.Math.Between(0 + wallA.width / 2, width - wallA.width / 2))
+        
+        wallA.body.setCollideWorldBounds(true);
+        wallA.body.setVelocityX(400);
+        wallA.body.setBounce(1); 
+        wallA.body.setImmovable(true);
 
         let wallB = this.physics.add.sprite(0,height/2, 'wall')
         wallB.setX(Phaser.Math.Between(0 + wallB.width /2, width - wallB.width /2))
@@ -50,8 +69,13 @@ class Play extends Phaser.Scene {
         // add one-way
         this.oneWay = this.physics.add.sprite(0,height/4 * 3 , 'oneway')
         this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width /2, width - this.oneWay.width /2))
+        this.oneWay.body.setCollideWorldBounds(true)
+        this.oneWay.body.setVelocityX(300);
+        this.oneWay.body.setBounce(1); 
         this.oneWay.body.setImmovable(true)
         this.oneWay.body.checkCollision.down = false
+
+        this.oneWay.body.onWorldBounds = true;
 
         // add pointer input
         this.input.on('pointerdown', (pointer) => {
@@ -62,28 +86,17 @@ class Play extends Phaser.Scene {
             this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection_y)
 
         })
-
-        // cup/ball collision
-        this.physics.add.collider(this.ball, this.cup, (ball,cup)=>{
-            ball.destroy()
-        })
-
-        // ball/wall collision
-        this.physics.add.collider(this.ball, this.walls)
-
-        // ball/one-way collision
-        this.physics.add.collider(this.ball, this.oneWay)
-    }
+        BALLS()
+     }
 
     update() {
-
     }
 }
 /*
 CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
-[ ] Add ball reset logic on successful shot
-[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
+[X] Add ball reset logic on successful shot
+[X] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[X] Make one obstacle move left/right and bounce against screen edges
 [ ] Create and display shot counter, score, and successful shot percentage
 */
